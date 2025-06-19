12fa9853a2345372c412e6ae755afdcaadc39ab3
Felipe <felipe.martini2404@gmail.com>
2025-06-19 17:54:16 -0300

    finalzie ferramentas

--- Ferramentas.js
+++ Ferramentas.js
@@ -3,27 +3,31 @@
 
 import { Ferramenta } from "./Basicas.js";
 
+// Pá para cavar - ferramenta básica
 export class Pa extends Ferramenta {
   constructor() {
     super("pa");
   }
 }
 
+// Controle remoto com pilhas limitadas - 3 usos apenas
 export class ControleRemoto extends Ferramenta {
   #pilhas;
   #usos;
 
   constructor() {
     super("controle_remoto");
-    this.#pilhas = false;
+    this.#pilhas = false; // Inicia sem pilhas
     this.#usos = 0;
   }
 
+  // Adiciona pilhas e define 3 usos
   colocarPilhas() {
     this.#pilhas = true;
     this.#usos = 3;
   }
 
+  // Usa o controle - reduz usos disponíveis
   usar() {
     if (!this.#pilhas || this.#usos <= 0) {
       return false;
@@ -32,29 +36,34 @@ export class ControleRemoto extends Ferramenta {
     return true;
   }
 
+  // Getter para verificar se tem pilhas
   get temPilhas() {
     return this.#pilhas;
   }
 
+  // Getter para verificar usos restantes
   get usosRestantes() {
     return this.#usos;
   }
 }
 
+// Chave da casa - abre portas e armários
 export class ChaveCasa extends Ferramenta {
   constructor() {
     super("chave_casa");
   }
 }
 
+// Pé de cabra - uso único para forçar portas
 export class PeDeCabra extends Ferramenta {
   #usado;
 
   constructor() {
     super("pe_de_cabra");
-    this.#usado = false;
+    this.#usado = false; // Controla uso único
   }
 
+  // Pode ser usado apenas uma vez
   usar() {
     if (this.#usado) {
       return false;
@@ -64,6 +73,7 @@ export class PeDeCabra extends Ferramenta {
   }
 }
 
+// Relógio que mostra hora atual do jogo
 export class Relogio extends Ferramenta {
   #engine;
 
@@ -72,11 +82,13 @@ export class Relogio extends Ferramenta {
     this.#engine = engine;
   }
 
+  // Método para ver as horas atuais
   verHoras() {
     return this.#engine.horaAtual;
   }
 }
 
+// Chave especial do piso secreto - abre o sótão
 export class ChavePisoSecreto extends Ferramenta {
   constructor() {
     super("chave_piso_secreto");
